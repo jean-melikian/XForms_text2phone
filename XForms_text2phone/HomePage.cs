1f6a95685884b821d735b9ad1d0d09403def19a0
Jean-Christophe MELIKIAN <jean.melikian@gmail.com>
2017-01-07 17:17:35 +0100

    Init project

---
+++ XForms_text2phone/HomePage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace XForms_text2phone
+{
+	public partial class HomePage : ContentPage
+	{
+		public HomePage()
+		{
+			InitializeComponent();
+		}
+
+		public async void onButtonSubmitClicked(object sender, EventArgs e) 
+		{
+			var answer = await DisplayAlert("Translated !", "Would you like to call this number ?", "Yes", "No");
+			Debug.WriteLine("Answer: " + answer);
+		}
+	}
+}
