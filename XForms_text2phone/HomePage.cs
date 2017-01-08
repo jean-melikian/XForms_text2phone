10b5f2b97a7eabd640f870fc2965b081704562b9
Jean-Christophe MELIKIAN <jean.melikian@gmail.com>
2017-01-08 21:20:22 +0100

    Data binding + Entry char limitation + some basic validation

--- XForms_text2phone/HomePage.cs
+++ XForms_text2phone/HomePage.cs
@@ -4,25 +4,42 @@ using Xamarin.Forms;
 
 namespace XForms_text2phone {
 	public partial class HomePage : ContentPage {
-		
+
+		T9Model myDial;
+
 		public HomePage() {
 			InitializeComponent();
+			// DataBinding T9Model <=> HomePage UI
+			myDial = new T9Model();
+			BindingContext = myDial;
 		}
 
-		//private static IPhoneDialer PhoneDialer { get; set; }
+		void Handle_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e) {
+			// This is preventing the use from dialing an invalid phone number
+			if (myDial.Text.Length == T9Model.PHONE_NUMBER_LENGTH)
+				dialButton.IsEnabled = true;
+			else 
+				dialButton.IsEnabled = false;
+			/*
+			 * This check is already done on the model-side
+			 * But we need this to prevent the user from adding more characters to the Entry
+			*/
+			//if (textEntry.Text.Length > T9Model.PHONE_NUMBER_LENGTH)
+			//	textEntry.Text = textEntry.Text.Remove(textEntry.Text.Length - 1);
+		}
 
-		public async void onButtonSubmitClicked(object sender, EventArgs e) {
-			if (!String.IsNullOrEmpty(inputString.Text)) {
-				var input = inputString.Text;
-
-				var answer = await DisplayAlert("Translated !", "Would you like to call this number ?", "Yes", "No");
-				Debug.WriteLine("Answer: " + answer);
+		public async void onDialButtonClicked(object sender, EventArgs e) {
+			if (myDial.IsValid) {
+				var answer = await DisplayAlert(myDial.Number, "Would you like to call " + myDial.Number + " ?", "Yes", "No");
+				Debug.WriteLine("Dialing " + myDial.Number + ": " + answer);
 
 				if (answer)
-					DependencyService.Get<IPhoneDialer>().DialNumber(input);
+					DependencyService.Get<IPhoneDialer>().DialNumber(myDial.Number);
 				
-			} else
-				Debug.WriteLine("Field entry is null or empty !");
+			} else {
+				Debug.WriteLine("This phone number is invalid:" + myDial.Number);
+				Debug.WriteLine("Text from model: " + myDial.Text);
+			}
 		}
 	}
 }
---
+++ XForms_text2phone/T9Model.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace XForms_text2phone {
+	class T9Model : INotifyPropertyChanged {
+		public const int PHONE_NUMBER_LENGTH = 9;
+		
+		private String text = "";
+		private String number = "";
+		private bool isValid = false;
+		//private String remainingCharsLabel = "Saisissez un texte court(%d caractères max)";
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public T9Model() { }
+
+		public String Text {
+			set {
+				if (text != value) {
+					if(value.Length <= PHONE_NUMBER_LENGTH) {
+						text = value;
+						TextToNumber();
+					}
+					if (PropertyChanged != null) {
+						PropertyChanged(this, new PropertyChangedEventArgs("Text"));
+					}
+				}
+			}
+			get {
+				return text;
+			}
+		}
+
+		public String Number {
+			get {
+				return number;
+			}
+		}
+
+		public bool IsValid {
+			get {
+				return isValid;
+			}
+		}
+
+		private void TextToNumber() {
+			// TODO: Make the conversion and validateNumber
+			number = text;
+			validateNumber();
+			if (PropertyChanged != null) {
+				PropertyChanged(this, new PropertyChangedEventArgs("Number"));
+			}
+		}
+
+		private void validateNumber() {
+			// TODO: Make the validation and update isValid
+			if (text != "") {
+				if (text.Length == T9Model.PHONE_NUMBER_LENGTH) {
+					isValid = true;
+				}
+			}
+			isValid = false;
+		}
+	}
+}
