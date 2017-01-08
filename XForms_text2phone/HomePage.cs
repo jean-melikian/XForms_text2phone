c19862ce6b9b62ceabf4e8f089b87bebf03141f1
Jean-Christophe MELIKIAN <jean.melikian@gmail.com>
2017-01-08 02:58:28 +0100

    Implemented each platform's specific controls to dial

--- XForms_text2phone/HomePage.cs
+++ XForms_text2phone/HomePage.cs
@@ -2,19 +2,27 @@
 using System.Diagnostics;
 using Xamarin.Forms;
 
-namespace XForms_text2phone
-{
-	public partial class HomePage : ContentPage
-	{
-		public HomePage()
-		{
+namespace XForms_text2phone {
+	public partial class HomePage : ContentPage {
+		
+		public HomePage() {
 			InitializeComponent();
 		}
 
-		public async void onButtonSubmitClicked(object sender, EventArgs e) 
-		{
-			var answer = await DisplayAlert("Translated !", "Would you like to call this number ?", "Yes", "No");
-			Debug.WriteLine("Answer: " + answer);
+		//private static IPhoneDialer PhoneDialer { get; set; }
+
+		public async void onButtonSubmitClicked(object sender, EventArgs e) {
+			if (!String.IsNullOrEmpty(inputString.Text)) {
+				var input = inputString.Text;
+
+				var answer = await DisplayAlert("Translated !", "Would you like to call this number ?", "Yes", "No");
+				Debug.WriteLine("Answer: " + answer);
+
+				if (answer)
+					DependencyService.Get<IPhoneDialer>().DialNumber(input);
+				
+			} else
+				Debug.WriteLine("Field entry is null or empty !");
 		}
 	}
 }
---
+++ XForms_text2phone/IPhoneDialer.cs
@@ -0,0 +1,10 @@
+﻿// Documentation: https://developer.xamarin.com/guides/xamarin-forms/dependency-service/introduction/
+
+using System;
+namespace XForms_text2phone
+{
+	public interface IPhoneDialer
+	{
+		void DialNumber(string phoneNumber);
+	}
+}
